bc6444fbdabebd72e9861143780ef4bd5106a81d
akshataa22 <dhanawadeakshata53@gmail.com>
2023-11-08 00:53:07 +0530

    added EmployeeWageComputation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,38 +1,33 @@
 package Day3;
 
+import java.util.Random;
+
 public class EmployeeWage {
-        static final int workingDays = 20;
-        static final int empRatePerHour = 20;
         private static final int partTime = 1;
         private static final int fullTime = 2;
-        private static double empCheck = 0;
 
         public static void main(String[] args) {
+            final int workingDays = 20;
+            final int empRatePerHour = 20;
             int empHrs=0, empWage=0, totalEmpWage=0;    //initialising the values
             for (int day = 0; day < workingDays; day++) {
-                empCheck = Math.floor(Math.random() * 10) % 3;       //using random function to check attendance
-                System.out.println(empCheck);
+                int empCheck = new Random().nextInt(3);    //using random function to generate 0, 1 and 2
+                empHrs = getEmployeeHours(empCheck);
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
+                System.out.println("Day " + (day + 1) + " : Employee Wage: " + empWage);
             }
-
-            switch ((int) empCheck) {
-                case partTime:
-                    System.out.println("Employee is Present half Day");
-                    empHrs = 4;
-                    empWage = empHrs * empRatePerHour;
-                    System.out.println("Employee Wage: " + empWage);
-                    break;
-                case fullTime:
-                    System.out.println("Employee is Present full day");
-                    empHrs = 8;
-                    empWage = empHrs * empRatePerHour;
-                    System.out.println("Employee Wage: " + empWage);
-                    break;
-                default:
-                    empHrs = 0;
-                    System.out.println("Employee is absent.");
-            }
-
-            totalEmpWage = workingDays * empWage;
             System.out.println("Employee Wage for total working days: " + totalEmpWage);
+    }
+
+    private static int getEmployeeHours(int empCheck) {
+            switch (empCheck){
+                case partTime :
+                    return 4;
+                case fullTime:
+                    return 8;
+                default:
+                    return 0;
+            }
     }
 }
